affa43f42ba1a41c940bbfa755c8f248fe8820c2
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-10-24 16:23:47 +0500

    Repo setup with my first CRUD Workshop

---
+++ in React.js - by xplodivity/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DataTable from "./components/DataTable";
+
+function App() {
+  return (
+    <div>
+      <DataTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ in React.js - by xplodivity/src/components/DataTable.jsx
@@ -0,0 +1,9 @@
+'use server'
+
+export default function DataTable() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
